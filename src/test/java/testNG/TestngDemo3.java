c0b33779dd30e7a3148c7ffe71201d21bf357470
Umrao Kalyankar <Umrao Kalyankar@FAI-LAP-17>
2025-11-07 21:22:11 +0530

    This is first commit

---
+++ src/test/java/testNG/TestngDemo3.java
@@ -0,0 +1,59 @@
+package testNG;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestngDemo3 {
+	WebDriver driver;
+	WebElement userId;
+	WebElement password;
+	
+	@BeforeMethod
+	public void browserOpen()
+	{
+		String url = "https://www.facebook.com";
+	    driver = new EdgeDriver();
+		driver.get(url);
+		driver.manage().window().maximize();
+		userId = driver.findElement(By.id("email"));
+		password = driver.findElement(By.id("pass"));
+	}
+	
+	@Test(priority=4)
+	public void testcase1() throws InterruptedException
+	{
+		
+		userId.sendKeys("Umesh9@gmail.com");
+		Thread.sleep(2000);
+		password.sendKeys("Test@123");
+	}
+	
+	@Test(priority=2)
+	public void testcase2() throws InterruptedException
+	{
+		driver.findElement(By.id("email")).sendKeys("Harsh9@gmail.com");
+		Thread.sleep(2000);
+		driver.findElement(By.id("pass")).sendKeys("Test@123");
+	}
+	
+	@Test(priority=1)
+	public void testcase4() throws InterruptedException
+	{
+		driver.findElement(By.id("email")).sendKeys("Harsh91@gmail.com");
+		Thread.sleep(2000);
+		driver.findElement(By.id("pass")).sendKeys("Test@123");
+	}
+	
+	@AfterMethod
+	public void browserClose()
+	{
+		System.out.println(driver.getCurrentUrl());
+		driver.close();
+	}   
+
+}
---
+++ src/test/java/testNG/TestngDemo4.java
@@ -0,0 +1,72 @@
+package testNG;
+	
+	import org.openqa.selenium.By;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.chrome.ChromeDriver;
+
+	import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+	import org.testng.annotations.Test;
+
+	public class TestngDemo4 {
+		WebDriver driver;
+	@BeforeClass
+	public void classRun()
+	{
+		System.out.println("---Login test with Headless1 running now---");
+	}
+	@AfterClass
+	public void classStop()
+	{
+		System.out.println("---Login test with Headless1 stop now---");
+		driver.close();
+		
+	}
+	@Test
+	public void testcase1() throws InterruptedException 
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//input[@id = 'email']")).sendKeys("Umesh9pm@gmail.com");
+		
+	}
+	@Test
+	public void testcase2() throws InterruptedException 
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//input[@id = 'email']")).sendKeys("Umesh9pm@gmail.com");
+		
+	}
+	@Test
+	public void testcase3() throws InterruptedException 
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//input[@id = 'email']")).sendKeys("Umesh9pm@gmail.com");
+		
+	}
+	@Test
+	public void testcase4() throws InterruptedException 
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//input[@id = 'email']")).sendKeys("Umesh9pm@gmail.com");
+		
+	}
+	@Test
+	public void testcase5() throws InterruptedException 
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//input[@id = 'email']")).sendKeys("Umesh9pm@gmail.com");
+		
+	}
+	
+}
